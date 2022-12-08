995e990e0109c61d3eefc707c40f3696095ffa5c
ca r213.18 <ca.r213.18@ca.local>
2022-12-08 12:08:30 +0400

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+// function switcher() {
+//     const x = document.getElementById("cancel-hamburger");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+// kod niyese islemir 
