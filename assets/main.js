9558af9677808b4229ff48dd87fb581c7b1c3cae
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-12-09 00:56:15 +0400

    responsibility + dropdown + more functionalities

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,51 @@
 //     }
 //   }
 // kod niyese islemir 
+
+
+
+const bar = document.getElementById("hamburger");
+const cancelBar = document.getElementById("cancel-hamburger");
+const menu = document.getElementById("menu");
+
+bar.addEventListener('click', (x) => {
+    bar.style.display = "none"
+    cancelBar.style.display = "block"
+    menu.style.display = "flex"
+})
+
+cancelBar.addEventListener('click', (x) =>{
+    cancelBar.style.display="none"
+    bar.style.display="block"
+    menu.style.display="none"
+})
+/*
+bar.addEventListener('click', () => {
+
+    let data = new FormData(event.target)
+    if(data.get('password').length>5){
+        console.log(`İcazə verildi.`)
+    }else(
+        console.log(`Giriş qadağandır.`)
+    )
+    
+})
+
+*/
+
+const brandsList = document.getElementsByClassName("brands-list")
+const brandBtn = document.getElementById("brand")
+
+brandBtn.addEventListener('click', (x) => {
+    brandsList.style.display="flex"
+})
+
+// cancelBar.addEventListener('click', (x) =>{
+//     brandsList.style.display="none"
+// })
+
+const colaLogo = document.getElementById("coca-cola")
+
+colaLogo.addEventListener("click",function(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+})
